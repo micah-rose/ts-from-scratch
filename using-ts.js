66e221252635cf0007d90470caa1d2daf517337b
Micah Lund <micah.r.lund@gmail.com>
2021-05-26 21:48:58 -0600

    Tried to manually fix an error. Need to take a new approach.

--- using-ts.js
+++ using-ts.js
@@ -1,9 +1,10 @@
-var button = document.querySelector("button");
-var input1 = document.getElementById("num1");
-var input2 = document.getElementById("num2");
+var btn = document.querySelector("button");
+var inputA = document.getElementById("num1");
+var inputB = document.getElementById("num2");
 function add(num1, num2) {
     return num1 + num2;
 }
-button.addEventListener("click", function () {
-    console.log(add(+input1.value, +input2.value));
+btn.addEventListener("click", function () {
+    console.log(add(+inputA.value, +inputB.value));
 });
+console.log(add(+inputA.value, +inputB.value));
