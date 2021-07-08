df70d94a0cc5472463b5a4990480f09bfbac852e
Micah Lund <micah.r.lund@gmail.com>
2021-07-07 20:39:03 -0600

    Updated add function to using an arrow function.

--- using-ts.ts
+++ using-ts.ts
@@ -2,7 +2,7 @@ var button = document.querySelector("button") as HTMLButtonElement;
 var inputA = document.getElementById("num1")! as HTMLInputElement;
 var inputB = document.getElementById("num2")! as HTMLInputElement;
 
-function add(num1: number, num2: number){
+const add = (num1: number, num2: number) => {
     return num1 + num2;
 };
 
