cb9fe19bee4e6f16c421af30f8b5818ebb8ef27d
Micah Lund <micah.r.lund@gmail.com>
2021-07-08 17:26:55 -0600

    Forgot to compile my TypeScript to update the JS file.

--- using-ts.js
+++ using-ts.js
@@ -1,10 +1,9 @@
 var button = document.querySelector("button");
 var inputA = document.getElementById("num1");
 var inputB = document.getElementById("num2");
-function add(num1, num2) {
+var add = function (num1, num2) {
     return num1 + num2;
-}
-;
+};
 button.addEventListener("click", function () {
     console.log(add(+inputA.value, +inputB.value));
 });
