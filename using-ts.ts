0a2a648374ff86c74a33e42f0d131bf9332845d7
Micah Lund <micah.r.lund@gmail.com>
2021-07-09 14:39:56 -0600

    Changed variable declarations to using const.

--- using-ts.ts
+++ using-ts.ts
@@ -1,6 +1,6 @@
-var button = document.querySelector("button") as HTMLButtonElement;
-var inputA = document.getElementById("num1")! as HTMLInputElement;
-var inputB = document.getElementById("num2")! as HTMLInputElement;
+const button = document.querySelector("button") as HTMLButtonElement;
+const inputA = document.getElementById("num1")! as HTMLInputElement;
+const inputB = document.getElementById("num2")! as HTMLInputElement;
 
 const add = (num1: number, num2: number) => {
     return num1 + num2;
