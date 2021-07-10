423a94accb1e5fcecff70c1146e0bcd786b1f4b1
Micah Lund <micah.r.lund@gmail.com>
2021-07-10 11:57:56 -0600

    Imported JS file vs TS file in the html index, also changed variable names to match the instructor's code for consistency.

--- using-ts.ts
+++ using-ts.ts
@@ -1,13 +1,13 @@
 const button = document.querySelector("button") as HTMLButtonElement;
-const inputA = document.getElementById("num1")! as HTMLInputElement;
-const inputB = document.getElementById("num2")! as HTMLInputElement;
+const input1 = document.getElementById("num1")! as HTMLInputElement;
+const input2 = document.getElementById("num2")! as HTMLInputElement;
 
 const add = (num1: number, num2: number) => {
     return num1 + num2;
 };
 
 button.addEventListener("click", function() {
-    console.log(add(+inputA.value, +inputB.value));
+    console.log(add(+input1.value, +input2.value));
 });
 
-console.log(add(+inputA.value, +inputB.value));
+console.log(add(+input1.value, +input2.value));
