03cd5a13b1c659da05552011f793c40ba742963c
Micah Lund <micah.r.lund@gmail.com>
2021-07-28 16:38:43 -0600

    Did an exercise using booleans in TypeScript.

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-function add(n1, n2) {
-    console.log(typeof number1);
-    return n1 + n2;
+function add(n1, n2, showResult) {
+    if (showResult === true) {
+        console.log(n1 + n2);
+    }
+    else {
+        return n1 + n2;
+    }
 }
 var number1 = 5;
 var number2 = 2.8;
-var result = add(number1, number2);
-console.log(result);
+var printResult = true;
+add(number1, number2, printResult);
