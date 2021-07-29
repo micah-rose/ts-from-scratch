717e0a5ed29f57d5e3cec7c606f1f7e06b089db9
Micah Lund <micah.r.lund@gmail.com>
2021-07-29 17:17:03 -0600

    Added a new parameter to our add function.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-function add(n1, n2, showResult) {
+function add(n1, n2, showResult, phrase) {
     if (showResult === true) {
-        console.log(n1 + n2);
+        console.log(phrase + n1 + n2);
     }
     else {
         return n1 + n2;
@@ -9,4 +9,5 @@ function add(n1, n2, showResult) {
 var number1 = 5;
 var number2 = 2.8;
 var printResult = true;
-add(number1, number2, printResult);
+var resultPhrase = 'Result is: ';
+add(number1, number2, printResult, resultPhrase);
