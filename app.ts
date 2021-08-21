b42bf9adee755142d46d0ea097f8c0a41f402321
Micah Lund <micah.r.lund@gmail.com>
2021-08-21 17:35:49 -0600

    Added an enum to display how to use them within TypeScript.

--- app.ts
+++ app.ts
@@ -1,14 +1,33 @@
-const person: {
-    name: string;
-    age: number;
-    hobbies: string[];
-    role: [number, string];
-} = {
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string];
+// } = {
+//     name: 'Max',
+//     age: 30,
+//     hobbies: ['Sports', 'Cooking'],
+//     role: [2, 'author']
+// };
+
+enum Role {
+    ADMIN, READ_ONLY, AUTHOR
+}
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 3;
+
+const person = {
     name: 'Max',
     age: 30,
     hobbies: ['Sports', 'Cooking'],
-    role: [2, 'author']
+    role: Role.ADMIN
 };
+
+if(person.role === Role.ADMIN) {
+    console.log('is admin');
+}
 
 // person.role.push('admin');
 // person.role[1] = 10;
