d8e8728023db98df7a163f7aa360a92117566dea
Micah Lund <micah.r.lund@gmail.com>
2021-08-22 15:23:38 -0600

    Compiled TS to the JS file.

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string];
+// } = {
+//     name: 'Max',
+//     age: 30,
+//     hobbies: ['Sports', 'Cooking'],
+//     role: [2, 'author']
+// };
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 3;
 var person = {
     name: 'Max',
     age: 30,
     hobbies: ['Sports', 'Cooking'],
-    role: [2, 'author']
+    role: Role.ADMIN
 };
+if (person.role === Role.ADMIN) {
+    console.log('is admin');
+}
 // person.role.push('admin');
 // person.role[1] = 10;
 //person.role = [0, 'admin', 'user'];
