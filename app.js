8d1ee3694c2305cfaf902266646b34462030e911
Micah Lund <micah.r.lund@gmail.com>
2021-08-25 18:19:36 -0600

    Compiled TS code to the JS file.

--- app.js
+++ app.js
@@ -15,9 +15,6 @@ var Role;
     Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
     Role[Role["AUTHOR"] = 2] = "AUTHOR";
 })(Role || (Role = {}));
-// const ADMIN = 0;
-// const READ_ONLY = 1;
-// const AUTHOR = 3;
 var person = {
     name: 'Max',
     age: 30,
