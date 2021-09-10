661f5b955142b184ac0c71241e551baf30e91ad0
Micah Lund <micah.r.lund@gmail.com>
2021-09-09 21:45:42 -0600

    Improved parameters format.

--- app.ts
+++ app.ts
@@ -1,4 +1,8 @@
-function combine(input1: number | string, input2: number | string, resultConversion: string) {
+function combine(
+    input1: number | string,
+    input2: number | string,
+    resultConversion: string
+) {
     let result;
     if (typeof input1 === 'number' && typeof input2 === 'number') {
         result = input1 + input2
