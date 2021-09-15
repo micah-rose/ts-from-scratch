e7f0291cb401bad688db99414913ea6b8f87f58c
Micah Lund <micah.r.lund@gmail.com>
2021-09-15 16:40:04 -0600

    Cleaned up our result conversion to work as expected.

--- app.ts
+++ app.ts
@@ -4,16 +4,17 @@ function combine(
     resultConversion: string
 ) {
     let result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        result = +input1 + +input2
     } else {
         result = input1.toString() + input2.toString()
     }
-    if (resultConversion === 'as-number'){
-        return +result;
-    } else {
-        return result.toString();
-    }
+    return result;
+    // if (resultConversion === 'as-number'){
+    //     return +result;
+    // } else {
+    //     return result.toString();
+    // }
 }
 
 const combinedAges = combine(30, 26, 'as-number');
