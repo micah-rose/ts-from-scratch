9558c1d429a566b88169b0955605429a483aa661
Micah Lund <micah.r.lund@gmail.com>
2021-11-14 22:02:53 -0700

    Added a new function for next example.

--- app.ts
+++ app.ts
@@ -6,6 +6,10 @@ function printResult(num: number): void {
     console.log('Result: ' + num);
 }
 
+function addAndHandle(n1: number, n2: number, cb) {
+    //function body
+}
+
 printResult(add(5, 2));
 
 let combineValues: (a: number, b: number) => number;
