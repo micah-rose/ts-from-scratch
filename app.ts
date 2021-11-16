137ab745136943a10bf8f720dec0a8c73d460322
Micah Lund <micah.r.lund@gmail.com>
2021-11-16 15:29:17 -0700

    Added an example with setting types within a function and callback.

--- app.ts
+++ app.ts
@@ -6,8 +6,9 @@ function printResult(num: number): void {
     console.log('Result: ' + num);
 }
 
-function addAndHandle(n1: number, n2: number, cb) {
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
     const result = n1 + n2;
+    cb(result);
 }
 
 printResult(add(5, 2));
@@ -21,3 +22,8 @@ combineValues = add;
 console.log(combineValues(8, 8));
 
 // let someValue: undefined;
+
+addAndHandle(10, 20, (result) => {
+    console.log(result);
+    return result;
+});
