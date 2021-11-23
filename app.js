dab81dd6e6fd076007528494c03d1d80a5447b50
Micah Lund <micah.r.lund@gmail.com>
2021-11-22 17:01:23 -0700

    Compiled TS code.

--- app.js
+++ app.js
@@ -1,20 +1,3 @@
-function add(n1, n2) {
-    return n1 + n2;
-}
-function printResult(num) {
-    console.log('Result: ' + num);
-}
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-printResult(add(5, 2));
-var combineValues;
-combineValues = add;
-// combineValues = printResult;
-// combineValues = 5;
-console.log(combineValues(8, 8));
-// let someValue: undefined;
-addAndHandle(10, 20, function (result) {
-    console.log(result);
-});
+var userInput;
+userInput = 5;
+userInput = 'Max';
